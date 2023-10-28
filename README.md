ba8ebc3c156a0b32057eca8ea4a3da83141d1ce9
Alessio <alessiowright@outlook.com>
2023-10-28 21:00:27 +0100

    working

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # image-to-excel
-# image-to-excel
--- excel.py
+++ excel.py
@@ -2,29 +2,42 @@ from openpyxl import Workbook
 from openpyxl.styles import Color, PatternFill, Font, Border
 from PIL import Image
 
-redFill = PatternFill(start_color='FFFF0000',
-                   end_color='FFFF0000',
-                   fill_type='solid')
+
+def getColour(cols):
+    hexVals = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+    hexCol = 'FF'
+
+    for col in cols:
+        hexCol += hexVals[col//16] + hexVals[col % 16]
+        
+    return hexCol
+
+
+
+def getX(x):
+    letter = ''
+    while x > 25:   
+        letter += chr(65 + int((x)/26) - 1)
+        x = x - (int((x)/26))*26
+    letter += chr(65 + (int(x)))
+    return letter
 
 wb = Workbook()
 ws = wb.active
 
-# ws['A1'] = 2
-# ws['A1'].fill = redFill
 
 
-img = Image.open("img.jpg")
+img = Image.open("img1.jpg")
 im = img.load()
 for x in range(img.size[0]):
     for y in range(img.size[1]):
-        col = im[x,y]
 
-        hexCol = 'FF'
-        for i in range(3):
-            hexCol += hex(col[0])[2:]
-        print(hexCol)
-        excelCol = redFill = PatternFill(start_color=hexCol,
-                   end_color=hexCol,
-                   fill_type='solid')
+        hexCol = getColour(im[x,y])        
+        excelCol = PatternFill(start_color=hexCol,
+            end_color=hexCol,
+            fill_type='solid')
+        
+        cell = getX(x) + str(y+1)
+        ws[cell].fill = excelCol
 
 wb.save("test.xlsx")
